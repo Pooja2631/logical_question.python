52a991d9bfa8df20627269e088c46fbfda064ac2
Pooja2631 <prani305356@gmail.com>
2022-02-14 16:15:57 +0530

    first commit

--- list3.py
+++ list3.py
@@ -1,11 +1,12 @@
 list1=['deepti','chauhan']
 i=0
-capital=list1[i]
+string=""
 while i<len(list1):
     j=0
     while j<len(list1[i]):
-        if list1[i][j]==0:
-            capital=list1[i][j].upper()
+        if list1[i][j]==list1[i][0]:
+           capital=list1[i][j].upper()
+           string=string+"."+capital
         j=j+1
     i=i+1
-    print(capital)
+print(string[1:])
